04d9844f6f6fc9fe18b359a60a151c2a660649c7
HotDogMaster <99787639+HotDogMaster@users.noreply.github.com>
2022-10-07 13:56:22 +0900

    Add files via upload

---
+++ js/detail.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".ups").click(function(){
+        $(".detail_in_detail").css({'transform':'translateY(0)'});
+    })
+    $(".line").click(function(){
+        $(".detail_in_detail").css({'transform':'translateY(350px)'});
+    })
+})
