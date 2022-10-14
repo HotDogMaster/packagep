fd28998a78894d3ef4a8af1127514a4968a8aa6d
HotDogMaster <99787639+HotDogMaster@users.noreply.github.com>
2022-10-14 15:46:04 +0900

    Add files via upload

--- js/detail.js
+++ js/detail.js
@@ -5,4 +5,18 @@ $(function(){
     $(".line").click(function(){
         $(".detail_in_detail").css({'transform':'translateY(350px)'});
     })
+    var FirstScroll = 0;	
+    var prevScrollTop = $(window).scrollTop(),
+    tp = $(window).scrollTop();
+    $(window).scroll(function(e){
+        tp = $(this).scrollTop();
+        if ( tp > prevScrollTop ){
+            $("footer").addClass("footdown");
+            $("footer").removeClass("footup");
+    }else if( tp < prevScrollTop ) {
+        $("footer").addClass("footup");
+        $("footer").removeClass("footdown");
+    }
+    prevScrollTop = tp;
+});
 })
